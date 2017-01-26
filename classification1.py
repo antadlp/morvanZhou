d2f478afe329da73ee70db60644df0ca2d9c664c
antadlp <ant.adlp1986@gmail.com>
2017-01-26 16:27:24 -0600

    thoug life

--- classification1.py
+++ classification1.py
@@ -1,3 +1,28 @@
 import tensorflow as tf
 
-def add_
+import tensorflow.examples.tutorials.mnist \
+        import input_data
+
+mnist = input_data.read_data_sets('MNIST_data', \
+        one_hot=True)
+
+
+def add_layer(inputs, in_size, out_size, \
+        activation_function=None,):
+    #add one more layer and return the output of 
+    #this layer
+
+    Weights = tf.Variable(tf.random_normal([in_size, \
+            out_size]))
+
+    biases = tf.Variable(tf.zeros([1, out_size]) + 0.1,)
+    Wx_plus_b = tf.matmul(inputs, Weights) + biases
+
+    if activation_function is None:
+        outputs = Wx_plus_b
+    else:
+        outputs = activation_function(Wx_plus_b,)
+    return outputs
+
+
+

2674482f63caae131d1185643babbcaa091d4e78
antadlp <ant.adlp1986@gmail.com>
2017-04-28 20:17:03 -0500

    Exploring CNN example, first approach

--- video19-CNN.py
+++ video19-CNN.py
@@ -48,6 +48,23 @@ b_conv1 = bias_variable([32])
 h_conv1 = tf.nn.relu(conv2d(x_image, W_conv1) + b_conv1) # output size 28x28x32
 h_pool1 = max_pool_2x2(h_conv1)
 
+# conv2 layer ###
+W_conv2 = weight_variable([5, 5, 32, 64]) #patch 5x5, in size 32, out size 64
+b_conv2 = bias_variable([64])
+h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2) # output size 14x14x64
+h_pool2 = max_pool_2x2(h_conv2)                          # output size 7x7x64
+
+##fc1 layer ##
+W_fc1 = weight_variable([7*7*64*1024])
+b_fc1 = bias_variable([1024])
+#[n_samples, 7, 7, 64] ->> [n_samples, 7*7*64]
+h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*64])
+h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
+h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
+
+
+
+
 
 
 

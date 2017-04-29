21aeee584ff3c6e204adeaf85c2d0cb85b92b69c
antadlp <ant.adlp1986@gmail.com>
2017-04-28 20:45:57 -0500

    Exploring CNN example, first approach

--- video19-CNN.py
+++ video19-CNN.py
@@ -9,13 +9,13 @@ from tensorflow.examples.tutorials.mnist import input_data
 
 mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
 
-def comput_accuracy(v_xs, v_ys):
+def compute_accuracy(v_xs, v_ys):
    global prediction
-    y_pre = sess.run(prediction, feed_dict={xs: v_xs, keep_prob: 1})
-    correct_prediction = tf.equal(tf.argmax(y_pre,1), tf.argmax(v_ys,1))
-    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
-    result = sess.run(accuracy, feed_dict={xs: v_xs, ys: v_ys, keep_prob: 1})
-    return result
+   y_pre = sess.run(prediction, feed_dict={xs: v_xs, keep_prob: 1})
+   correct_prediction = tf.equal(tf.argmax(y_pre,1), tf.argmax(v_ys,1))
+   accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+   result = sess.run(accuracy, feed_dict={xs: v_xs, ys: v_ys, keep_prob: 1})
+   return result
 
 def weight_variable(shape):
     initial = tf.truncated_normal(shape, stddev=0.1)
@@ -55,19 +55,24 @@ h_conv2 = tf.nn.relu(conv2d(h_pool1, W_conv2) + b_conv2) # output size 14x14x64
 h_pool2 = max_pool_2x2(h_conv2)                          # output size 7x7x64
 
 ##fc1 layer ##
-W_fc1 = weight_variable([7*7*64*1024])
+W_fc1 = weight_variable([7*7*64, 1024])
 b_fc1 = bias_variable([1024])
 #[n_samples, 7, 7, 64] ->> [n_samples, 7*7*64]
 h_pool2_flat = tf.reshape(h_pool2, [-1, 7*7*64])
 h_fc1 = tf.nn.relu(tf.matmul(h_pool2_flat, W_fc1) + b_fc1)
 h_fc1_drop = tf.nn.dropout(h_fc1, keep_prob)
 
+##fc2 layer ##
+W_fc2 = weight_variable([1024, 10])
+b_fc2 =bias_variable([10])
+prediction = tf.nn.softmax(tf.matmul(h_fc1_drop, W_fc2) + b_fc2)
 
 
 
-
-
-
+# the error between prediction and real data
+cross_entropy = tf.reduce_mean(-tf.reduce_sum(ys * tf.log(prediction),
+                                              reduction_indices=[1]))       # loss
+train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
 
 sess = tf.Session()
 # important step
@@ -84,8 +89,7 @@ for i in range(1000):
     batch_xs, batch_ys = mnist.train.next_batch(100)
     sess.run(train_step, feed_dict={xs: batch_xs, ys: batch_ys, keep_prob: 0.5})
     if i % 50 == 0:
-        print(compute_accuracy(
-            mnist.test.images, mnist.test.labels))
+        print(compute_accuracy(mnist.test.images, mnist.test.labels))
 
 
 
@@ -100,15 +104,5 @@ for i in range(1000):
 
 
 
-
-
-
-
-
-
-# the error between prediction and real data
-cross_entropy = tf.reduce_mean(-tf.reduce_sum(ys * tf.log(prediction),
-                                              reduction_indices=[1]))       # loss
-train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
 
 
